ab8f169951636c82c9fd48aaae33431bf76fd36a
Dmytro Zykov <dmytri.zykov@gmail.com>
2021-07-10 01:25:23 +0300

    Added top-down control

--- Assets/Scripts/Player/Topdown Control/PlayerTopdownMovementContoller.cs
+++ Assets/Scripts/Player/Topdown Control/PlayerTopdownMovementContoller.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class PlayerTopdownMovementContoller : MonoBehaviour
 {
     PlayerTopdownController playerController;
+    
+    private Vector2 velocity = Vector2.zero;
 
-    void Awake()
+    private void Awake()
     {
         playerController = GetComponent<PlayerTopdownController>();
+    }
+
+    private void Update() 
+    {
+        velocity = playerController.playerInput.input.normalized * playerController.playerSpeed;
+    }
+
+    private void FixedUpdate() 
+    {
+        playerController.rbody.velocity = velocity;
     }
 }
